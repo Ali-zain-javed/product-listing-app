be998289fa930266aabb878939ce7e5332ddd00f
alizain113 <ali.zain@appifytech.com>
2023-10-23 19:17:53 +0500

    initianl commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import { getProducts } from "./services/productService";
+import ProductList, { Product, CartItem } from "./components/ProductList";
+import FilterOptions from "./components/FilterOptions";
 
-function App() {
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      if (data) {
+        setProducts(data);
+        setFilteredProducts(data);
+      }
+    });
+  }, []);
+
+  const handleFilterChange = useCallback(
+    (colour: string) => {
+      if (colour === "") {
+        setFilteredProducts(products);
+      } else {
+        const filtered = products.filter(
+          (product: Product) => product.colour === colour
+        );
+        setFilteredProducts(filtered);
+      }
+    },
+    [products, setFilteredProducts]
+  );
+
+  const handleIncrement = useCallback(
+    (productId: number) => {
+      setCartItems((prevCart) => {
+        const updatedCart = [...prevCart];
+        const itemIndex = updatedCart.findIndex(
+          (item: CartItem) => item.id === productId
+        );
+        if (itemIndex >= 0) {
+          updatedCart[itemIndex].qty++;
+        } else {
+          let findItem = products.find(
+            (product: Product) => product.id === productId
+          );
+          if (findItem) {
+            let NewItem = {
+              id: findItem.id,
+              name: findItem.name,
+              price: findItem.price,
+              qty: 1,
+            };
+            updatedCart.push(NewItem);
+          }
+        }
+        return updatedCart;
+      });
+    },
+    [setCartItems, products]
+  );
+
+  const handleDecrement = useCallback(
+    (productId: number) => {
+      setCartItems((prevCart) => {
+        const updatedCart = [...prevCart];
+        const itemIndex = updatedCart.findIndex(
+          (item) => item.id === productId
+        );
+        if (itemIndex >= 0 && updatedCart[itemIndex].qty > 0) {
+          updatedCart[itemIndex].qty--;
+        }
+        return updatedCart;
+      });
+    },
+    [setCartItems]
+  );
+
+  const handleRemoveFromCart = useCallback(
+    (productId: number) => {
+      setCartItems((prevCart) =>
+        prevCart.filter((item) => item.id !== productId)
+      );
+    },
+    [setCartItems]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Product Listing</h1>
+      <FilterOptions onFilterChange={handleFilterChange} />
+      <ProductList
+        products={filteredProducts}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        onRemove={handleRemoveFromCart}
+        cartItems={cartItems}
+      />
     </div>
   );
-}
+};
 
 export default App;
